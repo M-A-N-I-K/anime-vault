461ab6867041b852a33372ce42df461d096bdc80
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-03-22 18:33:04 +0530

    added animations

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { MotionDiv } from "./MotionDiv";
 export interface AnimeProp {
 	id: string;
 	name: string;
@@ -17,9 +17,25 @@ interface Prop {
 	index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+	hidden: {
+		opacity: 0,
+	},
+	visible: {
+		opacity: 1,
+	},
+};
+
+function AnimeCard({ anime, index }: Prop) {
 	return (
-		<div className="max-w-sm rounded relative w-full">
+		<MotionDiv
+			variants={variants}
+			initial="hidden"
+			animate="visible"
+			transition={{ duration: 0.5, delay: index * 0.1, ease: "easeInOut" }}
+			viewport={{ amount: 0 }}
+			className="max-w-sm rounded relative w-full"
+		>
 			<div className="relative w-full h-[37vh]">
 				<Image
 					src={`https://shikimori.one${anime.image.original}`}
@@ -66,7 +82,7 @@ function AnimeCard({ anime }: Prop) {
 					</div>
 				</div>
 			</div>
-		</div>
+		</MotionDiv>
 	);
 }
 

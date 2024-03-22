48bc0d1fe35108a12b529c9a68c3cdbe548c1a4c
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2024-03-22 19:02:45 +0530

    bg hero image changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 function Hero() {
 	return (
-		<header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
-			<div className="flex-1 flex flex-col gap-10">
+		<header className="relative bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
+			<div className="absolute inset-0 bg-black bg-opacity-50"></div>
+			<div className="flex-1 flex flex-col gap-10 relative z-10">
 				<Image
 					src="./logo.svg"
 					alt="logo"
@@ -16,11 +17,12 @@ function Hero() {
 					of Anime Magic
 				</h1>
 			</div>
-			<div className="lg:flex-1 relative w-full h-[50vh] justify-center">
+			<div className="lg:flex-1 relative w-full h-[50vh] justify-center z-0">
 				<Image
 					src="/anime.png"
 					alt="anime"
-					fill
+					layout="fill"
+					objectFit="contain"
 					className="object-contain"
 				/>
 			</div>
